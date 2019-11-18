b90352c3c2f24c39e6fd7c6db3fdcf5b68aead84
StephenTHendrix <stephenthendrix@gmail.com>
2019-11-18 13:04:50 -0600

    rerevert

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+// import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Jumbotron from "./components/Jumbotron";
 import Nav from "./components/Nav";
